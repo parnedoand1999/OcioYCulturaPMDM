207f782dad0cae05e3257af84e813c94e42326c3
parnedoand1999 <parnedoand@educacion.navarra.es>
2020-11-03 16:06:51 +0100

    Lista de nombres , concretamente de los 3 primeros para poder poner sliders y que no quede una aplicacion muy larga

---
+++ lib/screens/listanombres_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:ocio_y_cultura/providers/ocio_providers.dart';
+
+class ListaNombresScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nombres "),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return FutureBuilder(
+      future: ocioycultura.cargarNombres(),
+      initialData: [],
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            children: _listaElementos(snapshot.data),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  List<Widget> _listaElementos(List<String> data) {
+    final List<Widget> lst = [];
+    data.forEach((element) {
+      final w = ListTile(
+        title: Text(element),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {},
+      );
+      lst.add(w);
+      lst.add(Divider());
+    });
+    return lst;
+  }
+}
